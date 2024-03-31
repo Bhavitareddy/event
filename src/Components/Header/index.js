4fefcaea2d0d742094783401a6d498a6d6fb584f
bhavita <bhavitareddy1001@gamail.com>
2024-03-31 13:15:17 +0000

    solution

---
+++ src/Components/Header/index.js
@@ -0,0 +1,39 @@
+import React from {Component}
+
+ <nav class="navbar navbar-expand-lg navbar-light bg-white">
+        <div class="container">
+
+            <a class="navbar-brand" href="#">
+                BookNow
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav ml-auto">
+                    <a class="nav-link active" id="navItem1" href="#">
+                        Live Shows
+                        <span class="sr-only">(current)</span>
+                    </a>
+                    <a class="nav-link" href="#" id="navItem2">Streams</a>
+                    <a class="nav-link" href="#" id="navItem3">Movies</a>
+                    <a class="nav-link" href="#" id="navItem4">Plays</a>
+                    <a class="nav-link" href="#" id="navItem4">Events</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+    <div class="banner-section-bg-container">
+        <div class="text-center">
+            <h1 class="heading  mt-4">
+                Discover Exciting Events<br />
+                Happening Near You-<br />
+                Stay Tuned for Updates!
+            </h1>
+            <p class="para">Dorem ipsum dolor sit amet,consectetue<br />
+                adipiscing elit,Nunc vulputate llbero et<br />
+                velt interdum,ac</p>
+
+        </div>
+
+    </div>
